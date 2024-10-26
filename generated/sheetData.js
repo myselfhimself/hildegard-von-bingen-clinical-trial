4c22a0f2ebcd0922a4e86d6038f004b99c53353b
myselfhimself <myselfhimself@users.noreply.github.com>
2024-10-26 13:31:57 +0000

    Rebuilding cache from medical database

--- generated/sheetData.js
+++ generated/sheetData.js
@@ -1,7 +1,7 @@
-/* Content automatically generated with grabLatestSheetData.js on Wed Oct 23 2024 21:02:19 GMT+0000 (Coordinated Universal Time).*/
+/* Content automatically generated with grabLatestSheetData.js on Sat Oct 26 2024 13:31:55 GMT+0000 (Coordinated Universal Time).*/
 /* Load this into your main JS file, it will provide a global 'sheetData' object variable with keys: 'serology', 'periods', 'events', 'refRanges'. */
 if(typeof sheetData == "undefined") {var sheetData = [];}
-/* Last period end date: unknown */ sheetData["periods"] = [];
 /* Last blood-marker date: unknown */ sheetData["serology"] = [];
 /* Last medical event date: unknown */ sheetData["events"] = [];
 sheetData["refRanges"] = [];
+/* Last period end date: unknown */ sheetData["periods"] = [];
